ea8b50a180e38e1575e359a008e6d5f9ff1c97d4
kim-dahun <117079872+kim-dahun@users.noreply.github.com>
2023-09-21 15:09:17 +0900

    20230912-02 PureJava -> Spring

--- SpringStudy1/src/main/java/com/selfstudy/springtheory/web/LogDemoController.java
+++ SpringStudy1/src/main/java/com/selfstudy/springtheory/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.selfstudy.springtheory.web;
 import com.selfstudy.springtheory.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoSerivce logDemoSerivce;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
 
         String requestURL = request.getRequestURL().toString();
-
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- SpringStudy1/src/main/java/com/selfstudy/springtheory/web/LogDemoSerivce.java
+++ SpringStudy1/src/main/java/com/selfstudy/springtheory/web/LogDemoSerivce.java
@@ -2,15 +2,18 @@ package com.selfstudy.springtheory.web;
 
 import com.selfstudy.springtheory.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoSerivce {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     public void logic(String id){
+
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
 
         myLogger.log("service id = " + id);
 
